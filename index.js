7aed49a904ec0c67bb2a4aa8e95d7d4cb675adda
jgood13 <joseph.good8@gmail.com>
2022-07-27 08:38:20 -0600

    took off the style sheet from the generated html since im not using it

--- index.js
+++ index.js
@@ -137,7 +137,6 @@ addHtml = () => {
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
         crossorigin="anonymous"
       />
-      <link rel="stylesheet" href="./dist/style.css" />
     </head>
     <body>
       <header>
